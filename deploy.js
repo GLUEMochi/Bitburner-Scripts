65cdec193174fc34bab1b79e957b4a0cac111fad
Mochi <kopparvid@gmail.com>
2023-03-28 22:53:20 +0200

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,10 +1,33 @@
+import { spider } from "scripts/spider.js";
+import { seeker } from "scripts/seeker.js";
 /** @param {NS} ns */
 export async function main(ns) {
+	spider(ns);
 	ns.tail();
-	const target = "joesguns";
+	ns.disableLog('ALL');
+	const target = seeker(ns);
+	const exa = "exa_mk1.js";
+	let totalThreads = [];
+	var exaRAM = ns.getScriptRam(exa);
+	let hosts = ns.read("hackableServers.txt").split(",");
 
-	ns.nuke(target);
-	ns.scp("exa_mk1.js",target);
-	ns.exec("exa_mk1.js",target,6,"joesguns");
+	for (let i = 0; i < hosts.length; i++) {
+		let hostname = hosts[i];
+		let threadCount = Math.floor((ns.getServerMaxRam(hostname)-ns.getServerUsedRam(hostname)) / exaRAM);
+		
+		ns.killall(hostname);
+		
+		if(ns.fileExists("BruteSSH.exe","home")){ns.brutessh(hostname);}
+		
+		ns.nuke(hostname);
+		
+		await ns.scp(exa, hostname);
 
+		ns.exec(exa, hostname, threadCount, target);
+
+		totalThreads.push(threadCount);
+
+		
+	}
+	ns.tprint("All current scripts killed, exas deployed to "+target+".");
 }
