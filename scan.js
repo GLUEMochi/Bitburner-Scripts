33690ec4e14be842499b415b560dedf6d27d9c4c
Mochi <kopparvid@gmail.com>
2023-03-28 22:54:10 +0200

    Update scan.js

--- scan.js
+++ scan.js
@@ -1,4 +1,3 @@
-
 import { spider } from "scripts/spider.js";
 /** @param {NS} ns */
 export async function main(ns) {
@@ -35,7 +34,6 @@ export async function main(ns) {
 	
 	ns.print("---------------")
 	ns.print("> Hacking Level: " + ns.getHackingLevel() + "\n");
-	ns.print("> Hacked servers: " + hacked.length + " out of " + hackable.length + " hackable.\n");
 	//Hacked
 	for (let i = 0; i < hacked.length; i++) {
 		let serv = hacked[i];
@@ -45,10 +43,12 @@ export async function main(ns) {
 		let secmin = ns.getServerMinSecurityLevel(serv);
 		let ram = ns.getServerMaxRam(serv);
 		ns.print(serv + "\n");
+		ns.print("RAM- "+ ns.formatRam(ram));
+		if(ns.getServerMaxMoney(serv)!=0){
 		ns.print("$--- " + " $"+ns.formatNumber(money) + " / " +" $"+ns.formatNumber(maxMoney));
 		ns.print("Sec- " + ns.formatNumber(sec) + " / "+ns.formatNumber(secmin));
-		ns.print("RAM- "+ ns.formatRam(ram));
-		ns.print(ns.formatPercent(Math.floor((money/maxMoney)*100)));
+		ns.print(ns.formatPercent((money/maxMoney)));
+		}
 	}
 	//Hackable
 }
