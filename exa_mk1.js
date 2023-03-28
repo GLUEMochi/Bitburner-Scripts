68b3e68ed0421a1413a87e0568f0217d1da7c654
Mochi <kopparvid@gmail.com>
2023-03-28 19:36:38 +0200

    Create exa_mk1.js

---
+++ exa_mk1.js
@@ -0,0 +1,14 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    const args = ns.flags([['server','joesguns']]);
+    const hostname = args[0];
+    while (true) {
+        if (ns.getServerSecurityLevel(hostname) > ns.getServerMinSecurityLevel(hostname)) {
+            await ns.weaken(hostname);
+        } else if (ns.getServerMoneyAvailable(hostname) < ns.getServerMaxMoney(hostname)) {
+            await ns.grow(hostname);
+        } else {
+            await ns.hack(hostname);
+        }
+    }
+}
