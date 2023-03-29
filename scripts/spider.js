afd5476e228a46247c1f9aeb4541c7226cd556d1
Mochi <kopparvid@gmail.com>
2023-03-29 22:59:10 +0200

    Update spider.js

--- scripts/spider.js
+++ scripts/spider.js
@@ -3,12 +3,12 @@ export function spider(ns) {
     // +1 to tools for each new kind of port we can open. 
     // This is just to keep from having to write the same script 5 times.
     let tools = 1
-    if (ns.fileExists("BruteSSH.exe", 	"home")) { tools = tools + 1; }
-	if (ns.fileExists("FTPCrack.exe", 	"home")) { tools = tools + 1; }
-	if (ns.fileExists("replaySMTP.exe",	"home")) { tools = tools + 1; }
-	if (ns.fileExists("HTTPWorm.exe",	"home")) { tools = tools + 1; }
-	if (ns.fileExists("SQLInject.exe", 	"home")) { tools = tools + 1; }
-    
+    if (ns.fileExists("BruteSSH.exe", "home")) { tools = tools + 1; }
+    if (ns.fileExists("FTPCrack.exe", "home")) { tools = tools + 1; }
+    if (ns.fileExists("replaySMTP.exe", "home")) { tools = tools + 1; }
+    if (ns.fileExists("HTTPWorm.exe", "home")) { tools = tools + 1; }
+    if (ns.fileExists("SQLInject.exe", "home")) { tools = tools + 1; }
+
     // Populate list of all servers
     let serversSeen = ['home']
     for (let i = 0; i < serversSeen.length; i++) {
@@ -34,7 +34,12 @@ export function spider(ns) {
         ns.write("Servers_Ports_" + i + ".txt", ports, "w");
     }
 
-    //Get list of accessible servers
+    //Populate list of my own purchased servers
+    let backbrain = ns.getPurchasedServers().tostring;
+    ns.write("purchasedServers.txt", backbrain, "w");
+
+    //Get list of hacked servers
+    //Fix this shit to work for my own servers or something, it currently does fuck all
     let hacked = [];
     for (let i = 0; i < serversSeen.length; i++) {
         if (ns.hasRootAccess(serversSeen[i]) == true) {
@@ -42,27 +47,22 @@ export function spider(ns) {
         }
     }
 
-    let hackedList = hacked.toString();
-    ns.write("hackedServers.txt", hackedList, "w");
-
     //Get list of hackable servers.
     let hackable = [];
     let hackLvl = ns.getHackingLevel();
-    
+
     for (let i = 0; i < tools; i++) {
-		ns.print("finding servers with "+i+" ports.")
-		let port = ns.read("Servers_Ports_"+i+".txt");
-		let listPort = port.split(",")
-		for (let i = 0; i < listPort.length; i++) {
-			const serv = listPort[i];
-			if (ns.getServerRequiredHackingLevel(serv) <= hackLvl) {
-				hackable.push(listPort[i]);
-			}
-			ns.print("Looping...")
-			ns.print(hackable)
-		}
-	}
-    
+        ns.print("finding servers with " + i + " ports.")
+        let port = ns.read("Servers_Ports_" + i + ".txt");
+        let listPort = port.split(",")
+        for (let i = 0; i < listPort.length; i++) {
+            const serv = listPort[i];
+            if (ns.getServerRequiredHackingLevel(serv) <= hackLvl) {
+                hackable.push(listPort[i]);
+            }
+        }
+    }
+
     let hackableList = hackable.toString();
     ns.write("hackableServers.txt", hackableList, "w");
 
